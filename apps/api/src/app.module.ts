1733be8aaf750155e070449b618a647e13367d27
NelakaWith <nelaka.with@gmail.com>
2026-01-30 10:10:12 +0530

    chore(api): dependency fixed

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PulseModule } from './pulse/pulse.module';
 
 @Module({
-  imports: [PulseModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'black_box_beacon',
+      entities: [],
+      synchronize: true, // Set to false in production
+    }),
+    PulseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
