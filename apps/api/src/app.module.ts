0df29374ecdbb0f4b3711de11e2780457e2fdb90
NelakaWith <nelaka.with@gmail.com>
2026-01-30 10:02:13 +0530

    feat(api): implement Pulse module with controller and service, add tests

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PulseModule } from './pulse/pulse.module';
 
 @Module({
-  imports: [],
+  imports: [PulseModule],
   controllers: [AppController],
   providers: [AppService],
 })
